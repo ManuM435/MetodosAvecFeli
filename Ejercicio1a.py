50ca82f61e8e121d57913fd88bb1a39c41a631da
Felicitas <fmarolda@udesa.edu.ar>
2024-03-24 16:11:18 -0300

    commitear

--- Ejercicio1a.py
+++ Ejercicio1a.py
@@ -1,4 +1,5 @@
 #Ejercicio 1a
+#INTERPOLACIÓN CON PUNTOS EQUIESPACIADOS
 #Interpolación lagrange
 import funcionesTP1
 import numpy as np
@@ -16,13 +17,13 @@ equi_points_1a = np.linspace(-4,4,9)
 eval_points_1a = funcionesTP1.eval_points_lister(equi_points_1a, funcion1a)
 
 # Paso 3: Creamos una funcion matematica en base a nuestro "ground truth" para interpolar
-def funcionInterpol1a(x):
+def funcionInterpol1aLagrange(x):
     return funcionesTP1.lagrange_interpolation(x, equi_points_1a, eval_points_1a)
 
 # Paso 4: Graficamos la función original y la interpolada
 x = np.linspace(-4, 4, 1000)  # Más puntos para una mejor visualización
 y_original = [funcion1a(xi) for xi in x]
-y_interpolada = [funcionInterpol1a(xi) for xi in x]
+y_interpolada = [funcionInterpol1aLagrange(xi) for xi in x]
 
 # TODO: Descomentar para visualizar (y borrar esta linea)
 # plt.plot(x, y_original, label='Función original')
@@ -34,17 +35,17 @@ y_interpolada = [funcionInterpol1a(xi) for xi in x]
 # plt.show()
 
 #Paso 5: Calculamos el error absoluto
-error_absoluto = [abs(funcion1a(xi) - funcionInterpol1a(xi)) for xi in x]
+error_absoluto_lagrange = [abs(funcion1a(xi) - funcionInterpol1aLagrange(xi)) for xi in x]
 
 # TODO: Descomentar para visualizar (y borrar esta linea)
-# plt.plot(x, error_absoluto)
+# plt.plot(x, error_absoluto_lagrange)
 # plt.xlabel('x')
 # plt.ylabel('Error absoluto')
 # plt.title('Error absoluto en interpolación Lagrange')
 # plt.show()
 
 #Paso 6: Calculamos el error relativo
-error_relativo = [abs(funcion1a(xi) - funcionInterpol1a(xi)) / abs(funcion1a(xi)) for xi in x]
+error_relativo = [abs(funcion1a(xi) - funcionInterpol1aLagrange(xi)) / abs(funcion1a(xi)) for xi in x]
 
 # TODO: Descomentar para visualizar (y borrar esta linea)
 # plt.plot(x, error_relativo)
@@ -65,12 +66,78 @@ def error_vs_cantidad_puntos(funcion, x, cantidad_puntos):
 
 cantidad_puntos = range(2, 17)
 error = error_vs_cantidad_puntos(funcion1a, x, cantidad_puntos)
-plt.plot(cantidad_puntos, error)
-for i, txt in enumerate(error):
-    plt.annotate(f'{int(txt)}', (cantidad_puntos[i], error[i]))
-    plt.plot([cantidad_puntos[i], cantidad_puntos[i]], [0, error[i]], 'k--')  # Add pointed lines
-plt.xticks(cantidad_puntos)
-plt.xlabel('Cantidad de puntos')
-plt.ylabel('Error')
-plt.title('Error en interpolación Lagrange en función de la cantidad de puntos')
-plt.show()
+# plt.plot(cantidad_puntos, error)
+# for i, txt in enumerate(error):
+#     plt.annotate(f'{int(txt)}', (cantidad_puntos[i], error[i]))
+#     plt.plot([cantidad_puntos[i], cantidad_puntos[i]], [0, error[i]], 'k--')  # Add pointed lines
+# plt.xticks(cantidad_puntos)
+# plt.xlabel('Cantidad de puntos')
+# plt.ylabel('Error')
+# plt.title('Error en interpolación Lagrange en función de la cantidad de puntos')
+# plt.show()
+
+#AHORA LO MISMO PERO CON SPLINES
+#Paso 1: creamos lista de puntos equiespacidos para usar como "ground truth"
+equi_points_1a = np.linspace(-4,4,16)
+# print(equi_points_1a)
+
+# Paso 2: Luego de crear la lista de puntos, evaluamos la función en cada punto
+eval_points_1a = funcionesTP1.eval_points_lister(equi_points_1a, funcion1a)
+
+# Paso 3: Definimos una función que cree funciones cúbicas entre los puntos que cumplas las condiciones de splines
+def funcionInterpol1aSplines(x):
+    return funcionesTP1.spline_interpolation(x, equi_points_1a, eval_points_1a)
+
+# Paso 4: Graficamos la función original y la interpolada
+x = np.linspace(-4, 4, 1000)  # Más puntos para una mejor visualización
+y_originalSplines = [funcion1a(xi) for xi in x]
+y_interpoladaSplines = [funcionInterpol1aSplines(xi) for xi in x]
+# TODO: Descomentar para visualizar (y borrar esta linea)
+# plt.plot(x, y_originalSplines, label='Función original')
+# plt.plot(x, y_interpoladaSplines, label='Función interpolada')
+# plt.legend()
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Interpolación Splines')
+# plt.show()
+
+#Paso 5: Calculamos el error absoluto
+error_absoluto_splines = [abs(funcion1a(xi) - funcionInterpol1aSplines(xi)) for xi in x]
+#TODO: Descomentar para visualizar (y borrar esta linea)
+# plt.plot(x, error_absoluto_splines)
+# plt.xlabel('x')
+# plt.ylabel('Error absoluto')
+# plt.title('Error absoluto en interpolación Splines')
+# plt.show()
+
+#Paso 6: Calculamos el error relativo
+error_relativo_splines = [abs(funcion1a(xi) - funcionInterpol1aSplines(xi)) / abs(funcion1a(xi)) for xi in x]
+
+#TODO: Descomentar para visualizar (y borrar esta linea)
+# plt.plot(x, error_relativo_splines)
+# plt.xlabel('x')
+# plt.ylabel('Error relativo')
+# plt.title('Error relativo en interpolación Splines')
+# plt.show()
+
+#Paso 7: Hacemos una función para calcular el error dependiendo de la cantidad de puntos que elegimos para interpolar y graficamos
+def error_vs_cantidad_puntos_splines(funcion, x, cantidad_puntos):
+    error = []
+    for n in cantidad_puntos:
+        equi_points = np.linspace(-4, 4, n)
+        eval_points = funcionesTP1.eval_points_lister(equi_points, funcion)
+        interpol = lambda x: funcionesTP1.spline_interpolation(x, equi_points, eval_points)
+        error.append(sum([abs(funcion(xi) - interpol(xi)) for xi in x]))
+    return error
+
+cantidad_puntos = range(2, 30)
+error = error_vs_cantidad_puntos_splines(funcion1a, x, cantidad_puntos)
+# plt.plot(cantidad_puntos, error)
+# for i, txt in enumerate(error):
+#     plt.annotate(f'{int(txt)}', (cantidad_puntos[i], error[i]))
+#     plt.plot([cantidad_puntos[i], cantidad_puntos[i]], [0, error[i]], 'k--')  # Add pointed lines
+# plt.xticks(cantidad_puntos)
+# plt.xlabel('Cantidad de puntos')
+# plt.ylabel('Error')
+# plt.title('Error en interpolación Splines en función de la cantidad de puntos')
+# plt.show()
--- funcionesTP1.py
+++ funcionesTP1.py
@@ -23,3 +23,62 @@ def lagrange_interpolation(x, list_x, list_y):
                 term *= (x - list_x[j]) / (list_x[i] - list_x[j])
         result += term
     return result
+
+#Función que reciba un par de puntos y crea una función cúbica que los una y que cumpla las condiciones de splines
+# def spline_cubico(x0,x1,y0,y1):
+#     a = y0
+#     b = (y1 - y0)/(x1 - x0)
+#     c = 0
+#     d = 0
+#     return lambda x: a + b*(x-x0) + c*(x-x0)**2 + d*(x-x0)**3
+def spline_cubico(x0, x1, y0, y1, m0, m1):
+    h = x1 - x0
+    a = y0
+    b = m0
+    c = (3*(y1 - y0)/h**2) - ((m1 + 2*m0)/h)
+    d = (2*(y0 - y1)/h**3) + ((m1 + m0)/h**2)
+    return lambda x: a + b*(x-x0) + c*(x-x0)**2 + d*(x-x0)**3
+
+# #Función que recibe una lista de puntos y una lista de valores de la función en esos puntos y devuelve una lista de funciones cúbicas que unen los puntos y cumplen las condiciones de splines
+# def spline_interpolation(x, list_x, list_y):
+#     n = len(list_x)
+#     splines = []
+#     for i in range(n-1):
+#         splines.append(spline_cubico(list_x[i], list_x[i+1], list_y[i], list_y[i+1]))
+#     for i in range(n-1):
+#         if x >= list_x[i] and x <= list_x[i+1]:
+#             return splines[i](x)
+#     return 0
+def spline_interpolation(x, list_x, list_y):
+    n = len(list_x)
+    h = [list_x[i+1] - list_x[i] for i in range(n-1)]
+    alpha = [(3*(list_y[i+1] - list_y[i])/h[i]) - (3*(list_y[i] - list_y[i-1])/h[i-1]) for i in range(1, n-1)]
+    l = [1] * (n-1)
+    mu = [0] * (n-1)
+    z = [0] * (n-1)
+    
+    for i in range(1, n-1):
+        l[i] = 2*(list_x[i+1] - list_x[i-1]) - h[i-1]*mu[i-1]
+        mu[i] = h[i]/l[i]
+        z[i] = (alpha[i-1] - h[i-1]*z[i-1])/l[i]
+    
+    l.append(1)
+    z.append(0)
+    c = [0] * n
+    b = [0] * n
+    d = [0] * n
+    
+    for j in range(n-2, -1, -1):
+        c[j] = z[j] - mu[j]*c[j+1]
+        b[j] = (list_y[j+1] - list_y[j])/h[j] - h[j]*(c[j+1] + 2*c[j])/3
+        d[j] = (c[j+1] - c[j])/(3*h[j])
+    
+    splines = []
+    for i in range(n-1):
+        splines.append(spline_cubico(list_x[i], list_x[i+1], list_y[i], list_y[i+1], b[i], b[i+1]))
+    
+    for i in range(n-1):
+        if x >= list_x[i] and x <= list_x[i+1]:
+            return splines[i](x)
+    
+    return 0
