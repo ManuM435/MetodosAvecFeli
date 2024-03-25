290b6bf6497ef7548e3ddecc2b2d42e06722c87a
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-03-25 02:42:25 -0300

    Organizado con Nueva Funcion Splines. Queda rehacer splines equiespaciados con las nuevas funciones, y refactorizar calculo de error

--- Ejercicio1a.py
+++ Ejercicio1a.py
@@ -77,32 +77,10 @@ error = error_vs_cantidad_puntos(funcion1a, x, cantidad_puntos)
 # plt.show()
 
 #AHORA LO MISMO PERO CON SPLINES
-#Paso 1: creamos lista de puntos equiespacidos para usar como "ground truth"
-equi_points_1a_splines = np.linspace(-4,4,16)
-# print(equi_points_1a)
-
-# Paso 2: Luego de crear la lista de puntos, evaluamos la función en cada punto
-eval_points_1a_splines = funcionesTP1.eval_points_lister(equi_points_1a_splines, funcion1a)
-
-# Paso 3: Definimos una función que cree funciones cúbicas entre los puntos que cumplas las condiciones de splines
-def funcionInterpol1aSplines(x):
-    return funcionesTP1.spline_interpolation(x, equi_points_1a_splines, eval_points_1a_splines)
-
-# Paso 4: Graficamos la función original y la interpolada
-x = np.linspace(-4, 4, 1000)  # Más puntos para una mejor visualización
-y_originalSplines = [funcion1a(xi) for xi in x]
-y_interpoladaSplines = [funcionInterpol1aSplines(xi) for xi in x]
-# TODO: Descomentar para visualizar (y borrar esta linea)
-# plt.plot(x, y_originalSplines, label='Función original')
-# plt.plot(x, y_interpoladaSplines, label='Función interpolada')
-# plt.legend()
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('Interpolación Splines')
-# plt.show()
+# TODO REHACER SPLINES CON EQUIESPACIADOS USANDO NUEVAS FUNCIONES
 
 #Paso 5: Calculamos el error absoluto
-error_absoluto_splines = [abs(funcion1a(xi) - funcionInterpol1aSplines(xi)) for xi in x]
+#error_absoluto_splines = 
 #TODO: Descomentar para visualizar (y borrar esta linea)
 # plt.plot(x, error_absoluto_splines)
 # plt.xlabel('x')
@@ -111,7 +89,7 @@ error_absoluto_splines = [abs(funcion1a(xi) - funcionInterpol1aSplines(xi)) for 
 # plt.show()
 
 #Paso 6: Calculamos el error relativo
-error_relativo_splines = [abs(funcion1a(xi) - funcionInterpol1aSplines(xi)) / abs(funcion1a(xi)) for xi in x]
+# error_relativo_splines =
 
 #TODO: Descomentar para visualizar (y borrar esta linea)
 # plt.plot(x, error_relativo_splines)
@@ -121,17 +99,17 @@ error_relativo_splines = [abs(funcion1a(xi) - funcionInterpol1aSplines(xi)) / ab
 # plt.show()
 
 #Paso 7: Hacemos una función para calcular el error dependiendo de la cantidad de puntos que elegimos para interpolar y graficamos
-def error_vs_cantidad_puntos_splines(funcion, x, cantidad_puntos):
-    error = []
-    for n in cantidad_puntos:
-        equi_points = np.linspace(-4, 4, n)
-        eval_points = funcionesTP1.eval_points_lister(equi_points, funcion)
-        interpol = lambda x: funcionesTP1.spline_interpolation(x, equi_points, eval_points)
-        error.append(sum([abs(funcion(xi) - interpol(xi)) for xi in x]))
-    return error
+# def error_vs_cantidad_puntos_splines(funcion, x, cantidad_puntos):
+#     error = []
+#     for n in cantidad_puntos:
+#         equi_points = np.linspace(-4, 4, n)
+#         eval_points = funcionesTP1.eval_points_lister(equi_points, funcion)
+#         interpol = lambda x: funcionesTP1.spline_interpolation(x, equi_points, eval_points)
+#         error.append(sum([abs(funcion(xi) - interpol(xi)) for xi in x]))
+#     return error
 
-cantidad_puntos = range(2, 30)
-error = error_vs_cantidad_puntos_splines(funcion1a, x, cantidad_puntos)
+# cantidad_puntos = range(2, 30)
+# error = error_vs_cantidad_puntos_splines(funcion1a, x, cantidad_puntos)
 # plt.plot(cantidad_puntos, error)
 # for i, txt in enumerate(error):
 #     plt.annotate(f'{int(txt)}', (cantidad_puntos[i], error[i]))
@@ -143,9 +121,10 @@ error = error_vs_cantidad_puntos_splines(funcion1a, x, cantidad_puntos)
 # plt.show()
 
 #AHORA CON PUNTOS NO EQUIESPACIADOS
+
 #Lagrange
 #Paso 1: creamos lista de puntos no equiespaciados para usar como "ground truth" usando los nodos de Chebyshev
-cheb_points_1a_lagrange = funcionesTP1.chebyshev_nodes(-4, 4, 15)
+cheb_points_1a_lagrange = funcionesTP1.Chebyshev(15)
 # print(cheb_points_1a)
 
 # Paso 2: Luego de crear la lista de puntos, evaluamos la función en cada punto
@@ -190,7 +169,7 @@ error_relativo_Chebyshev = [abs(funcion1a(xi) - funcionInterpol1aLagrangeNoEquie
 def error_vs_cantidad_puntos_no_equiespaciados(funcion, x, cantidad_puntos):
     error = []
     for n in cantidad_puntos:
-        cheb_points = funcionesTP1.chebyshev_nodes(-4, 4, n)
+        cheb_points = funcionesTP1.Chebyshev(n)
         eval_points = funcionesTP1.eval_points_lister(cheb_points, funcion)
         interpol = lambda x: funcionesTP1.lagrange_interpolation(x, cheb_points, eval_points)
         error.append(sum([abs(funcion(xi) - interpol(xi)) for xi in x]))
@@ -209,60 +188,21 @@ error = error_vs_cantidad_puntos_no_equiespaciados(funcion1a, x, cantidad_puntos
 # plt.show()
 
 
-# Con Splines
+# No Equiespaciados con Splines
 
-# Define the number of points
-n = 30  # Modify this value to change the number of nodes
-
-# Generar Nodos de Chebyshev
-x = np.cos(np.linspace(0, np.pi, n))
-x = 4 * x  # Multiplicar por 4 para extender de [-1, 1] a [-4, 4].
-
-# Sort x in ascending order and calculate the corresponding y values
-x = np.sort(x)
+# Establecer parametros para la interpolación
+x = funcionesTP1.Chebyshev(30)
 y = np.array([funcion1a(xi) for xi in x])
+splines = funcionesTP1.Cubic_Splines(x, y)
 
-# Create the cubic spline for each interval
-splines = []
-# Initialize m_prev and c_prev
-m_prev, c_prev = 0, 0
-
-def calculate_splines(x, y):
-    splines = []
-    m_prev, c_prev = 0, 0
-
-    for i in range(len(x) - 2):
-        h = x[i+1] - x[i]
-        h_next = x[i+2] - x[i+1]
-        d = 6 * ((y[i+2] - y[i+1]) / h_next - (y[i+1] - y[i]) / h)
-        a = h
-        b = 2 * (h + h_next)
-        c = h_next
-        d_next = 6 * ((y[i+3] - y[i+2]) / (x[i+3] - x[i+2]) - (y[i+2] - y[i+1]) / h_next) if i < len(x) - 3 else 0
-        m_next = (d - a * m_prev) / (b - a * c_prev) if i > 0 else d / b
-        splines.append((y[i], (y[i+1] - y[i]) / h - h * (2 * m_prev + m_next) / 6, m_prev / 2, (m_next - m_prev) / (6 * h)))
-        m_prev, c_prev = m_next, c
-
-    return splines
-
-# Calculate the splines
-splines = calculate_splines(x, y)
-
-# Plot the function and the interpolation
+# Plotear la funcion y la interpolacion
 x_plot = np.linspace(-4, 4, 1000)
 y_plot = np.array([funcion1a(xi) for xi in x_plot])
 plt.plot(x_plot, y_plot, label='Original function')
 
-def spline_interpolation(x, splines, x_values):
-    for i in range(len(splines)):
-        if x >= x_values[i] and x <= x_values[i+1]:
-            return splines[i][0] + splines[i][1] * (x - x_values[i]) + splines[i][2] * (x - x_values[i]) ** 2 + splines[i][3] * (x - x_values[i]) ** 3
-    return None
-
-
 for i in range(len(splines)):
     xi = np.linspace(x[i], x[i+1], 100)
-    y_spline = np.array([spline_interpolation(xi, splines, x) for xi in x_plot])
+    y_spline = np.array([funcionesTP1.spline_interpolation(xi, splines, x) for xi in x_plot])
 plt.plot(x_plot, y_spline, label='Cubic spline interpolation')
 
 plt.legend()
@@ -273,30 +213,3 @@ plt.show()
 
 
 
-# TODO ESTO PUEDE QUE SEA IRRELEVANT Y BORRABLE
-
-#Ahora con splines
-#Paso 1: creamos lista de puntos no equiespacidos para usar como "ground truth" usando los nodos de Chebyshev
-# cheb_points_1a_splines = funcionesTP1.chebyshev_nodes(-4, 4, 15)
-# print(cheb_points_1a_splines)
-
-# # Paso 2: Luego de crear la lista de puntos, evaluamos la función en cada punto
-# eval_points_1a_splines_cheb = funcionesTP1.eval_points_lister(cheb_points_1a_splines, funcion1a)
-
-# # Paso 3: Definsimos una función que cree funciones cúbicas entre los puntos que cumplas las condiciones de splines
-# def funcionInterpol1aSplinesNoEquiespaciados(x):
-#     return funcionesTP1.spline_interpolation(x, cheb_points_1a_splines, eval_points_1a_splines_cheb)
-
-# # Paso 4: Graficamos la función original y la interpolada
-# x_range = np.linspace(-4, 4, 50)
-# y_originalSplines_Chebyshev = [funcion1a(xi) for xi in x_range]
-# y_interpoladaSplines_Chebyshev = [funcionInterpol1aSplinesNoEquiespaciados(xi) for xi in x_range]
-
-# # graficar
-# plt.plot(x_range, y_originalSplines_Chebyshev, label='Función original')
-# plt.plot(x_range, y_interpoladaSplines_Chebyshev, label='Función interpolada')
-# plt.legend()
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('Interpolación Splines con nodos de Chebyshev')
-# plt.show()
--- funcionesTP1.py
+++ funcionesTP1.py
@@ -24,57 +24,45 @@ def lagrange_interpolation(x, list_x, list_y):
         result += term
     return result
 
+#TODO
+#Funcion para calcular el error absoluto
 
 
-# TODO LO DE ABAJO WILL PROBABLY BE IRRELEVANT, BORRAR
+#Funcion para calcular el error relativo
 
-# #Función que reciba un par de puntos y crea una función cúbica que los una y que cumpla las condiciones de splines
-# def spline_cubico(x0, x1, y0, y1, m0, m1):
-#     h = x1 - x0
-#     a = y0
-#     b = m0
-#     c = (3*(y1 - y0)/h**2) - ((m1 + 2*m0)/h)
-#     d = (2*(y0 - y1)/h**3) + ((m1 + m0)/h**2)
-#     return lambda x: a + b*(x-x0) + c*(x-x0)**2 + d*(x-x0)**3
 
-# # #Función que recibe una lista de puntos y una lista de valores de la función en esos puntos y devuelve una lista de funciones cúbicas que unen los puntos y cumplen las condiciones de splines
-# def spline_interpolation(x, list_x, list_y):
-#     n = len(list_x)
-#     h = [list_x[i+1] - list_x[i] for i in range(n-1)]
-#     alpha = [(3*(list_y[i+1] - list_y[i])/h[i]) - (3*(list_y[i] - list_y[i-1])/h[i-1]) for i in range(1, n-1)]
-#     l = [1] * (n-1)
-#     mu = [0] * (n-1)
-#     z = [0] * (n-1)
-    
-#     for i in range(1, n-1):
-#         l[i] = 2*(list_x[i+1] - list_x[i-1]) - h[i-1]*mu[i-1]
-#         mu[i] = h[i]/l[i]
-#         z[i] = (alpha[i-1] - h[i-1]*z[i-1])/l[i]
-    
-#     l.append(1)
-#     z.append(0)
-#     c = [0] * n
-#     b = [0] * n
-#     d = [0] * n
-    
-#     for j in range(n-2, -1, -1):
-#         c[j] = z[j] - mu[j]*c[j+1]
-#         b[j] = (list_y[j+1] - list_y[j])/h[j] - h[j]*(c[j+1] + 2*c[j])/3
-#         d[j] = (c[j+1] - c[j])/(3*h[j])
-    
-#     splines = []
-#     for i in range(n-1):
-#         splines.append(spline_cubico(list_x[i], list_x[i+1], list_y[i], list_y[i+1], b[i], b[i+1]))
-        
-#     for i in range(n-1):
-#         if x >= list_x[i] and x <= list_x[i+1]: 
-#             return splines[i](x)
-    
-#     return 0
+# Funciones de Interpolacion de Splines Cúbicos
+def Cubic_Splines(x, y):
+    splines = []
+    m_prev, c_prev = 0, 0
 
-# #Función que crea una lista de nodos de Chebyshev para una interpolación
-# def chebyshev_nodes(a, b, n):
-#     return [(a + b)/2 + ((b - a)/2)*math.cos((2*i + 1)*math.pi/(2*n)) for i in range(n)]
+    for i in range(len(x) - 2):
+        h = x[i+1] - x[i]
+        h_next = x[i+2] - x[i+1]
+        d = 6 * ((y[i+2] - y[i+1]) / h_next - (y[i+1] - y[i]) / h)
+        a = h
+        b = 2 * (h + h_next)
+        c = h_next
+        d_next = 6 * ((y[i+3] - y[i+2]) / (x[i+3] - x[i+2]) - (y[i+2] - y[i+1]) / h_next) if i < len(x) - 3 else 0
+        m_next = (d - a * m_prev) / (b - a * c_prev) if i > 0 else d / b
+        splines.append((y[i], (y[i+1] - y[i]) / h - h * (2 * m_prev + m_next) / 6, m_prev / 2, (m_next - m_prev) / (6 * h)))
+        m_prev, c_prev = m_next, c
+
+    return splines
+
+def spline_interpolation(x, splines, x_values):
+    for i in range(len(splines)):
+        if x >= x_values[i] and x <= x_values[i+1]:
+            return splines[i][0] + splines[i][1] * (x - x_values[i]) + splines[i][2] * (x - x_values[i]) ** 2 + splines[i][3] * (x - x_values[i]) ** 3
+    return None
+
+#Función que crea una lista de nodos de Chebyshev para una interpolación
+def Chebyshev(x):
+    x = np.cos(np.linspace(0, np.pi, x))
+    x = 4 * x  # Multiplicar por 4 para extender de [-1, 1] a [-4, 4].
+    x = np.sort(x)
+    return x
+
 
 #______________________________________________________________________________________________________
 #Ejercicio 1b
