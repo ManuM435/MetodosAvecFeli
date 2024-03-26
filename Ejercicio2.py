446ea6c1b823e388fa6e3b71ac4d91518b941708
Felicitas <fmarolda@udesa.edu.ar>
2024-03-26 13:35:37 -0300

    grafico de intersección

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -60,3 +60,42 @@ error_absoluto_y = [abs(funcionInterpol2LagrangeY(ti) - yi) for ti, yi in zip(t,
 
 error_relativo_x = [abs(funcionInterpol2LagrangeX(ti) - xi) / xi if xi != 0 else -1 for ti, xi in zip(t, x_ground_truth)]
 error_relativo_y = [abs(funcionInterpol2LagrangeY(ti) - yi) / yi if yi != 0 else -1 for ti, yi in zip(t, y_ground_truth)]
+
+#SEGUNDA PARTE --> interpolar el recorrido del otro vehículo y determinar si este pasó por algún lugar que haya pasado el tractor (va haber que usar métodos numéricos y búsqueda de raices de la intersección de las funciones)
+#Paso 1: Cargar los datos del archivo de mediciones del otro vehículo
+data_list_mediciones2 = []
+
+with open('mnyo_mediciones2.csv', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=' ')
+    
+    for row in csv_reader:
+        data_list_mediciones2.append(row)
+
+#Paso 1.2: Separar los datos en dos listas, una para x y otra para y
+x_mediciones2 = [float(row[0]) for row in data_list_mediciones2]
+y_mediciones2 = [float(row[1]) for row in data_list_mediciones2]
+
+#Paso 1.3: Crear una lista que represente el tiempo con el mismo largo que las listas de x e y
+t_mediciones2 = np.linspace(0, 100, len(x_mediciones2))
+
+#Paso 2: Crear una función matemática en base a las x mediciones (eje y) y t mediciones (eje x) interpolar usando lagrange
+def funcionInterpol2LagrangeX2(t):
+    return funcionesTP1.lagrange_interpolation(t, t_mediciones2, x_mediciones2)
+
+#Paso 3: Crear una función matemática en base a las y mediciones (eje y) y t mediciones (eje x) interpolar usando lagrange
+def funcionInterpol2LagrangeY2(t):
+    return funcionesTP1.lagrange_interpolation(t, t_mediciones2, y_mediciones2)
+
+#Paso 4: Graficar en el eje x lo que de funcionInterpol2LagrangeX2(t) y en el eje y lo que de funcionInterpol2LagrangeY2(t), poner en el mismo gráfico el recorrido del tractor interpolado y el ground truth
+
+x_interpol2 = [funcionInterpol2LagrangeX2(ti) for ti in t]
+y_interpol2 = [funcionInterpol2LagrangeY2(ti) for ti in t]
+
+plt.plot(x_interpol, y_interpol, label='Tractor Interpolado')
+plt.plot(x_ground_truth, y_ground_truth, label='Tractor Ground Truth')
+plt.plot(x_interpol2, y_interpol2, label='Vehículo 2 Interpolado')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interpolación de mediciones de tractor y vehículo 2')
+plt.show()
