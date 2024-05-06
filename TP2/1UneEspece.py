d677a46f5bc0f80cb5c930499f1c3108dc50f06c
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-06 20:11:14 -0300

    logistiquePopulation (J'espere)

--- TP2/1UneEspece.py
+++ TP2/1UneEspece.py
@@ -15,3 +15,6 @@ def logistiqueODE(r, N, K):
 def logistiquePopulation(r, N0, K, t):
     '''Calcule la population en fonction du temps t, avec un taux de croissance r, une capacité de charge K et une population initiale N0'''
     return K * N0 / (N0 + (K - N0) * np.exp(-r * t))
+
+
+
