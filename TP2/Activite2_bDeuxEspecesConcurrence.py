6236facad3a92b2caa81b3b5d5714a3b87a1f989
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-07 16:26:59 -0300

    funciona (maybe)

--- TP2/Activite2_bDeuxEspecesConcurrence.py
+++ TP2/Activite2_bDeuxEspecesConcurrence.py
@@ -4,59 +4,47 @@ import scipy.integrate as spi
 import fonctions_auxiliares as aux
 
 # Definimos las ecuaciones diferenciales
-def lotkaVolterra(y, r1, r2, K1, K2, alpha12, alpha21):
-    N1, N2 = y
+def lotkaVolterra(N1, N2, r1, r2, K1, K2, alpha12, alpha21):
     dN1dt = r1 * N1 * (1 - N1 / K1 - alpha12 * N2 / K1)
     dN2dt = r2 * N2 * (1 - N2 / K2 - alpha21 * N1 / K2)
     return [dN1dt, dN2dt]
 
-# Definimos los valores iniciales
-# TODO: Ajustar bien estos numeros
-y0, y1, y2, y3, y4, y5, y6, y7 = [10, 10], [10, 10], [10, 10], [10, 10], [10, 10], [10, 10], [10, 10], [10, 10]
+def rkSolve(ode, y0, t, h):
+    '''Resuelve el sistema de ecuaciones diferenciales con el método de Runge-Kutta'''
+    return aux.runge_kutta_4(ode, y0, t, h)
 
-# Definimos el tiempo
+# el time
 t = np.linspace(0, 100, 1000)
 
-# Resolvemos las ecuaciones diferenciales con runge kutta
-rkCase0 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y0, t)
-rkCase1 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y1, t)
-rkCase2 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y2, t)
-rkCase3 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y3, t)
-rkCase4 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y4, t)
-rkCase5 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y5, t)
-rkCase6 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y6, t)
-rkCase7 = aux.runge_kutta_4(lambda t, y: lotkaVolterra(y, 0.1, 0.1, 1000, 1000, 0.01, 0.01), y7, t)
+datos0 
+datos1
+datos2
 
-# Graficamos las soluciones
-plt.plot(t, rkCase0[:, 0], label='Especies 1')
-plt.plot(t, rkCase0[:, 1], label='Especies 2')
+
+h = 0.1
+r1 = 0.1
+r2 = 0.5
+K1 = 100
+K2 = 80
+alpha12 = 0.01
+alpha21 = 0.66
+
+# Condiciones iniciales
+y00, y01, y02, y03, y04, y05, y06, y07 = [10, 10], [10, 10], [10, 10], [10, 10], [10, 10], [10, 10], [10, 10], [10, 10]
+
+# Resolvemos el sistema de ecuaciones diferenciales con nuestro runge kutta
+y = np.array([y0])
+for i in range(len(t) - 1):
+    y0 = rkSolve(lambda y, t: lotkaVolterra(y[0], y[1], r1, r2, K1, K2, alpha12, alpha21), y00, t[i], h)
+    y = np.append(y, [y0], axis=0)
+
+# Graficamos
+plt.plot(t, y[:, 0], label='N1')
+plt.plot(t, y[:, 1], label='N2')
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.title('Population Over Time')
+
 plt.legend()
+
 plt.show()
-
-# Graficamos el campo de vectores
-r1 = 0.1
-r2 = 0.1
-K1 = 1000
-K2 = 1000
-alpha12 = 0.01
-alpha21 = 0.01
-
-N1 = np.linspace(0, 1000, 20)
-N2 = np.linspace(0, 1000, 20)
-N1, N2 = np.meshgrid(N1, N2)
-dN1dt = r1 * N1 * (1 - N1 / K1 - alpha12 * N2 / K1)
-dN2dt = r2 * N2 * (1 - N2 / K2 - alpha21 * N1 / K2)
-plt.quiver(N1, N2, dN1dt, dN2dt)
-plt.xlabel('N1')
-plt.ylabel('N2')
-plt.title('Population Vector Field')
-plt.show()
-
-# Puntos de equilibrio
-N1_eq = K1 * (1 - alpha21 * K2 / (r2 * K2))
-N2_eq = K2 * (1 - alpha12 * K1 / (r1 * K1))
-print(f'Puntos de equilibrio: ({N1_eq}, {N2_eq})')
-
--- TP2/fonctions_auxiliares.py
+++ TP2/fonctions_auxiliares.py
@@ -3,16 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as spi
 
-def runge_kutta_4(ecuaciones:list, y0, t):
+def runge_kutta_4(ode, y0, t, h):
     '''Implementa el método de Runge-Kutta de orden 4 para resolver ecuaciones diferenciales'''
-    n = len(t)
-    y = np.zeros((n, len(y0)))
-    y[0] = y0
-    for i in range(n-1):
-        h = t[i+1] - t[i]
-        k1 = h * ecuaciones(t[i], y[i])[0] and h*ecuaciones(t[i], y[i])[1]
-        k2 = h * ecuaciones(t[i] + h/2, y[i] + k1/2)[0] and h*ecuaciones(t[i] + h/2, y[i] + k1/2)[1]
-        k3 = h * ecuaciones(t[i] + h/2, y[i] + k2/2)[0] and h*ecuaciones(t[i] + h/2, y[i] + k2/2)[1]
-        k4 = h * ecuaciones(t[i] + h, y[i] + k3)[0] and h*ecuaciones(t[i] + h, y[i] + k3)[1]
-        y[i+1] = y[i] + (k1 + 2*k2 + 2*k3 + k4) / 6
-    return y
+    k1 = h * np.array(ode(y0, t))
+    k2 = h * np.array(ode(y0 + k1 / 2, t + h / 2))
+    k3 = h * np.array(ode(y0 + k2 / 2, t + h / 2))
+    k4 = h * np.array(ode(y0 + k3, t + h))
+    return y0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
