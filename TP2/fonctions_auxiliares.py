5d4c9a5993442ad26e6e8c099f510f70154aa8a6
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-09 15:58:35 -0300

    updated Euler & RK

--- TP2/fonctions_auxiliares.py
+++ TP2/fonctions_auxiliares.py
@@ -3,15 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as spi
 
-def euler_method(ode, y0, t, h):
-    '''Implementa el método de Euler para resolver ecuaciones diferenciales'''
-    return y0 + h * (ode(y0, t))
+def euler_method(ode_func, r, r_values, initial_N, step_size):
+    N_values = [initial_N]
+    for _ in range(len(r_values) - 1):  # Iterate over the length of r_values - 1
+        N_next = N_values[-1] + step_size * ode_func(r, N_values[-1])
+        N_values.append(N_next)
+    return N_values
 
-def runge_kutta_4(ode, y0, t, h):
-    '''Implementa el método de Runge-Kutta de orden 4 para resolver ecuaciones diferenciales'''
-    k1 = h * np.array(ode(y0, t))
-    k2 = h * np.array(ode(y0 + k1 / 2, t + h / 2))
-    k3 = h * np.array(ode(y0 + k2 / 2, t + h / 2))
-    k4 = h * np.array(ode(y0 + k3, t + h))
-    return y0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+def rk_4_A1(ode_func, r, N_values, step_size):
+    new_N_values = [N_values[-1]]
+    for _ in range(len(N_values) - 1):  # Iterate over the length of N_values - 1
+        k1 = ode_func(r, new_N_values[-1])
+        k2 = ode_func(r, new_N_values[-1] + step_size / 2 * k1)
+        k3 = ode_func(r, new_N_values[-1] + step_size / 2 * k2)
+        k4 = ode_func(r, new_N_values[-1] + step_size * k3)
+        N_next = new_N_values[-1] + step_size / 6 * (k1 + 2*k2 + 2*k3 + k4)
+        new_N_values.append(N_next)
+    return new_N_values
 
