daad13137e80d85c98e13b2f3372c8c131841658
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-09 20:28:29 -0300

    Acomodando datos del 2

--- TP2/Activite2_bDeuxEspecesConcurrence.py
+++ TP2/Activite2_bDeuxEspecesConcurrence.py
@@ -13,21 +13,31 @@ def rkSolve(ode, y0, t, h):
     '''Resuelve el sistema de ecuaciones diferenciales con el método de Runge-Kutta'''
     return aux.runge_kutta_4(ode, y0, t, h)
 
+def EquilibriumPoint(r1, r2, K1, K2, alpha12, alpha21):
+    '''Calcula el punto de equilibrio del sistema de ecuaciones diferenciales'''
+    N1 = K1 - alpha12 * (K2 - alpha21 * K1) / (1 - alpha12 * alpha21)
+    N2 = (K2 - alpha21 * K1) / (1 - alpha12 * alpha21)
+    return [N1, N2]
+
 # el time y su'estep
 t = np.linspace(0, 100, 1000)
 h = 0.1
 
 ''' datos = (N1, N2, r1, r2, K1, K2, alpha12, alpha21) '''
 # Son para el lotkaVolterra
-datos0 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
+
+pars0 = (0.1, 0.5, 100, 80, 0.01, 0.66)
 #TODO CAMBIAR ESTOS DATOS, PROBAR MODELOS, FIJARSE ESO DE '4 TIPOS DE GRAFICO' EN BASE A SUS VALORES DE NO SE QUE Y LAS ISOCLINAS??
-datos1 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
-datos2 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
-datos3 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
-datos4 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
-datos5 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
-datos6 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
-datos7 = (10, 10, 0.1, 0.5, 100, 80, 0.01, 0.66)
+pars1 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+pars2 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+pars3 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+pars4 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+pars5 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+pars6 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+pars7 = (0.1, 0.5, 100, 80, 0.01, 0.66)
+
+datos0 = (EquilibriumPoint(*pars0), pars0)
+# Hay que ver de separar los datos y que no esten en listas maybe
 
 
 r1 = 0.5
@@ -38,11 +48,7 @@ alpha12 = 1
 alpha21 = 10
 
 
-def EquilibriumPoint(r1, r2, K1, K2, alpha12, alpha21):
-    '''Calcula el punto de equilibrio del sistema de ecuaciones diferenciales'''
-    N1 = K1 - alpha12 * (K2 - alpha21 * K1) / (1 - alpha12 * alpha21)
-    N2 = (K2 - alpha21 * K1) / (1 - alpha12 * alpha21)
-    return [N1, N2]
+
 
 TheseEquis = (EquilibriumPoint(r1, r2, K1, K2, alpha12, alpha21))
 print(f"Los Puntos de Equilibrio son: {TheseEquis}")
