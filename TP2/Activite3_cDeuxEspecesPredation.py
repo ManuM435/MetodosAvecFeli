9b8009234ca35c7aafc2c09b83a9f54823e5f43d
Felicitas <fmarolda@udesa.edu.ar>
2024-05-09 15:27:25 -0300

    isoclinas

---
+++ TP2/Activite3_cDeuxEspecesPredation.py
@@ -0,0 +1,17 @@
+#Ejercicio 3
+import fonctions_auxiliares as aux
+
+def ODE(n,p,r,a,b,q):
+    dNdt = r*n - a*n*p
+    dPdt = b*n*p - q*p
+    return [dNdt, dPdt]
+
+def LVE(n,p,r,a,b,q,k):
+    dNdT = r*n*(1-n/k) - a*n*p
+    dPdt = b*n*p - q*p
+    return [dNdT, dPdt]
+
+def rkSolve(ode, y0, t, h):
+    '''Resuelve el sistema de ecuaciones diferenciales con el método de Runge-Kutta'''
+    return aux.runge_kutta_4(ode, y0, t, h)
+
