3ea5176391b053baa72d606912478ae14582c206
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-10 16:24:43 -0300

    Primeras Isoclinas ahora si

--- TP2/Activite2_bDeuxEspecesConcurrence.py
+++ TP2/Activite2_bDeuxEspecesConcurrence.py
@@ -23,6 +23,7 @@ def EquilibriumPoint(r1, r2, K1, K2, alpha12, alpha21):
 t = np.linspace(0, 100, 1000)
 h = 0.1
 
+# aca hacer todo lo demas del ejercicio, los datos y todo
 ''' datos = (N1, N2, r1, r2, K1, K2, alpha12, alpha21) '''
 # Son para el lotkaVolterra
 
@@ -37,57 +38,25 @@ pars6 = (0.1, 0.5, 100, 80, 0.01, 0.66)
 pars7 = (0.1, 0.5, 100, 80, 0.01, 0.66)
 
 datos0 = (EquilibriumPoint(*pars0), pars0)
-# Hay que ver de separar los datos y que no esten en listas maybe
 
-r1 = 0.5
-r2 = 0.75
+
+# Isoclinas Cero
+N1 = np.linspace(0, 100, 100)
+N2 = np.linspace(0, 200, 100)
 K1 = 100
-K2 = 80
-alpha12 = 1
-alpha21 = 10
+K2 = 200
+alpha12 = 1/2
+alpha21 = 2
 
-TheseEquis = (EquilibriumPoint(r1, r2, K1, K2, alpha12, alpha21))
-print(f"Los Puntos de Equilibrio son: {TheseEquis}")
+# Isoclinas
+isoclineN1 = K1 - alpha12 * N2
+isoclineN2 = K2 - alpha21 * N1
 
-# Isoclines 1
-K11, K12 = 100, 25
-alpha112, alpha121 = 4, 16
-
-N1_range1 = np.linspace(0, K11, 100)
-N2_range1 = np.linspace(0, K12, 100)
-
-iso11 = (K11 - N1_range1 - alpha112 * N2_range1) / (r1 * N1_range1)
-iso12 = (K12 - N2_range1 - alpha121 * N1_range1) / (r2 * N2_range1)
-
-# Plot the isoclines
-plt.plot(N1_range1, iso11, label='dN1/dt = 0')
-plt.plot(N2_range1, iso12, label='dN2/dt = 0')
+# Graficar
+plt.plot(N1, isoclineN1, label='Isocline of N1')
+plt.plot(isoclineN2, N2, label='Isocline of N2')
 plt.xlabel('N1')
 plt.ylabel('N2')
-plt.title('Isoclines 1')
 plt.legend()
-plt.grid(True)
 plt.show()
 
-# Isoclinas 2
-K21, K22 = 100, 25
-alpha212, alpha221 = 16, 4
-
-N1_range2 = np.linspace(0, K21, 100)
-N2_range2 = np.linspace(0, K22, 100)
-
-iso21 = (K21 - N1_range2 - alpha212 * N2_range2) / (r1 * N1_range2)
-iso22 = (K22 - N2_range2 - alpha221 * N1_range2) / (r2 * N2_range2)
-
-# Plot the isoclines
-plt.plot(N1_range2, iso21, label='dN1/dt = 0')
-plt.plot(N2_range2, iso22, label='dN2/dt = 0')
-plt.xlabel('N1')
-plt.ylabel('N2')
-plt.title('Isoclines 2')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-
-# ahora graficar con 25, 100 y 16, 4. Luego 25, 100 con 4, 16
