a443d4e9a07567474017e867e373cf19a1f4b061
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-14 15:05:40 -0300

    Runge Kutta Predator Prey done (creo)

--- TP2/Activite3_cDeuxEspecesPredation.py
+++ TP2/Activite3_cDeuxEspecesPredation.py
@@ -34,7 +34,7 @@ def rungeKuttaPredatorPrey(ode, n, p, r, a, b, q, dt, t_end):
         t += dt
     return n_values, p_values
 
-def rungeKuttaPredatorPreyExt(ode, n, p, r, a, b, q, k, dt, t_end):
+def rungeKuttaLotVolExt(ode, n, p, r, a, b, q, k, dt, t_end):
     t = 0
     n_values = [n]
     p_values = [p]
@@ -52,5 +52,25 @@ def rungeKuttaPredatorPreyExt(ode, n, p, r, a, b, q, k, dt, t_end):
         t += dt
     return n_values, p_values
 
-# Aproximar
+# Approximate Predator-Prey with Runge Kutta
 
+Pandas0 = 120  # initial population of prey species
+Tigers0 = 30  # initial population of predator species
+dt = 0.1 # time step size
+t_end = 10 # end time
+
+d1 = [0.2, 0.02, 0.004, 0.2] # Normal Values
+d2 = [0.2, 0.005, 0.004, 0.2] # Lower Predation aka Lower a
+d3 = [0.8, 0.02, 0.004, 0.2] # Highr Panda Growth Rate aka Lower r
+d4 = [0.2, 0.02, 0.024, 0.2] # Higher Predator Efficiency Rate aka Lower b
+d5 = [0.2, 0.02, 0.004, 0.6] # Higher Predator Death Rate aka Higher q
+
+pandas1, tigers1 = rungeKuttaPredatorPrey(PredatorPreyLotVol, Pandas0, Tigers0, d1[0], d1[1], d1[2], d1[3], dt, t_end)
+
+
+plt.plot(pandas1, label='Pandas Normal')
+plt.plot(tigers1, label='Predator Normal')
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.legend()
+plt.show()
