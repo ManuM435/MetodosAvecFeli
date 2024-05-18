f243aa3957f64a295d17b5aef054b0dd9b26f95f
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-18 03:09:37 -0300

    acomodadas algunas del 3

--- TP2/Activite3_cDeuxEspecesPredation.py
+++ TP2/Activite3_cDeuxEspecesPredation.py
@@ -111,8 +111,17 @@ axs[1, 1].set_xlabel('Time')
 axs[1, 1].set_ylabel('Population')
 axs[1, 1].legend(loc='upper right')
 
-plt.tight_layout()
+# Add a title to the entire figure
+fig.suptitle('Population Dynamics with Predator-Prey Model', fontsize=16)
+
+# Adjust layout to accommodate the figure title
+plt.tight_layout(rect=[0, 0, 1, 0.99])  # Adjust the rect to fit the suptitle
+
 plt.show()
+
+
+
+
 
 # Approximate Lotka Volterra Extended with Runge Kutta
 d1 = [0.2, 0.02, 0.004, 0.2, 200] # Normal Values
@@ -134,7 +143,9 @@ axs[0, 0].plot(pandas1, color='grey', linestyle=':', alpha=0.7, label='Pandas "N
 axs[0, 0].plot(leopards1, color='black', linestyle=':', alpha=0.7, label='Leopards "Normal"')
 axs[0, 0].plot(pandas2, color='blue', label='Pandas (Lower a)')
 axs[0, 0].plot(leopards2, color='red', label='Leopards2 (Lower a)')
-axs[0, 0].set_title('Population dynamics with a Lower Predation Rate')
+axs[0, 0].set_title('Pop. Dynamics with a Lower Predation Rate')
+axs[0, 0].set_xlabel('Time')
+axs[0, 0].set_ylabel('Population')
 axs[0, 0].legend(loc='upper right')
 
 # Subplot 2
@@ -143,6 +154,8 @@ axs[0, 1].plot(leopards1, color='black', linestyle=':', alpha=0.7, label='Leopar
 axs[0, 1].plot(pandas3, color='blue', label='Pandas (Higher r)')
 axs[0, 1].plot(leopards3, color='red', label='Leopards (Higher r)')
 axs[0, 1].set_title('Population dynamics with a Higher Panda Growth Rate')
+axs[0, 1].set_xlabel('Time')
+axs[0, 1].set_ylabel('Population')
 axs[0, 1].legend(loc='upper right')
 
 # Subplot 3
@@ -151,6 +164,8 @@ axs[1, 0].plot(leopards1, color='black', linestyle=':', alpha=0.7, label='Leopar
 axs[1, 0].plot(pandas4, color='blue', label='Pandas (Higher K)')
 axs[1, 0].plot(leopards4, color='red', label='Leopards (Higher K)')
 axs[1, 0].set_title('Population dynamics with a Higher Panda Carrying Capacity')
+axs[1, 0].set_xlabel('Time')
+axs[1, 0].set_ylabel('Population')
 axs[1, 0].legend(loc='upper right')
 
 # Subplot 4
@@ -159,9 +174,16 @@ axs[1, 1].plot(leopards1, color='black', linestyle=':', alpha=0.7, label='Leopar
 axs[1, 1].plot(pandas5, color='blue', label='Pandas (Lower K)')
 axs[1, 1].plot(leopards5, color='red', label='Leopards (Lower K)')
 axs[1, 1].set_title('Population dynamics with a Lower Panda Carrying Capacity')
+axs[1, 1].set_xlabel('Time')
+axs[1, 1].set_ylabel('Population')
 axs[1, 1].legend(loc='upper right')
 
-plt.tight_layout()
+# Add a title to the entire figure
+fig.suptitle('Population Dynamics with LVE Model', fontsize=16)
+
+# Adjust layout to accommodate the figure title
+plt.tight_layout(rect=[0, 0, 1, 0.99])  # Adjust the rect to fit the suptitle
+
 plt.show()
 
 
@@ -175,128 +197,128 @@ plt.show()
 
 
 
-#Isoclinas Cero
+# #Isoclinas Cero
 
-#Parámetros part 1
-Na = np.linspace(0, 10, 10)
-Pa = np.linspace(0, 10, 10)
-alphaa = 10
-betaa = 10
-ra = 0.1
-qa = 0.1
-alphaa = 0.1
-betaa = 0.1
+# #Parámetros part 1
+# Na = np.linspace(0, 10, 10)
+# Pa = np.linspace(0, 10, 10)
+# alphaa = 10
+# betaa = 10
+# ra = 0.1
+# qa = 0.1
+# alphaa = 0.1
+# betaa = 0.1
 
-#Isoclinas predador-presa
-isocline_Na = np.full_like(Na, qa/betaa)
-isocline_Pa = np.full_like(Pa, ra/alphaa)
+# #Isoclinas predador-presa
+# isocline_Na = np.full_like(Na, qa/betaa)
+# isocline_Pa = np.full_like(Pa, ra/alphaa)
 
-# ENcontrar pnuto de equilibrio
-equiPointA = aux.findEquilibriumPredatorPrey(alphaa, betaa, ra, qa)
-eq1, eq1b = equiPointA[0], equiPointA[1]
+# # ENcontrar pnuto de equilibrio
+# equiPointA = aux.findEquilibriumPredatorPrey(alphaa, betaa, ra, qa)
+# eq1, eq1b = equiPointA[0], equiPointA[1]
 
-#campo de vectores
-vN = np.linspace(0, 10, 40)
-vP = np.linspace(0, 10, 40)
-VN, VP = np.meshgrid(vN, vP)
+# #campo de vectores
+# vN = np.linspace(0, 10, 40)
+# vP = np.linspace(0, 10, 40)
+# VN, VP = np.meshgrid(vN, vP)
 
-dN = ra * VN - alphaa * VN * VP
-dP = alphaa * VN * VP - qa * VP
+# dN = ra * VN - alphaa * VN * VP
+# dP = alphaa * VN * VP - qa * VP
 
-#Trayectoria con runge kutta
-Na_values_1, Pa_values_1 = rungeKuttaPredatorPrey(PredatorPreyLotVol, 5, 1, ra, alphaa, betaa, qa, 0.1, 1000)
-Na_values_2, Pa_values_2 = rungeKuttaPredatorPrey(PredatorPreyLotVol, 1.5, 1.5, ra, alphaa, betaa, qa, 0.1, 1000)
-Na_values_3, Pa_values_3 = rungeKuttaPredatorPrey(PredatorPreyLotVol, 8, 0.6, ra, alphaa, betaa, qa, 0.1, 1000)
+# #Trayectoria con runge kutta
+# Na_values_1, Pa_values_1 = rungeKuttaPredatorPrey(PredatorPreyLotVol, 5, 1, ra, alphaa, betaa, qa, 0.1, 1000)
+# Na_values_2, Pa_values_2 = rungeKuttaPredatorPrey(PredatorPreyLotVol, 1.5, 1.5, ra, alphaa, betaa, qa, 0.1, 1000)
+# Na_values_3, Pa_values_3 = rungeKuttaPredatorPrey(PredatorPreyLotVol, 8, 0.6, ra, alphaa, betaa, qa, 0.1, 1000)
 
-#Graficos
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))  # Crear una figura con 2 gráficos uno al lado del otro
+# #Graficos
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))  # Crear una figura con 2 gráficos uno al lado del otro
 
-# Primer gráfico en ax1
-ax1.plot(Na, isocline_Na, label='Isocline of Pandas', color='indigo', linewidth=2, linestyle='--')
-ax1.plot(isocline_Pa, Pa, label='Isocline of Leopardos', color='limegreen', linewidth=2, linestyle='--')
-ax1.set_xlabel('P')
-ax1.set_ylabel('L')
-ax1.plot(Na_values_1, Pa_values_1, color='deeppink', label='Trayectoria 1')
-ax1.plot(Na_values_2, Pa_values_2, color='hotpink', label='Trayectoria 2')
-ax1.plot(Na_values_3, Pa_values_3, color='mediumvioletred', label='Trayectoria 3')
-ax1.set_title('Isoclines of Depredador y Presa')
-ax1.scatter(eq1, eq1b, color='red', label='Punto de Equilibrio', s=100, zorder=10)
-ax1.streamplot(vN, vP, dN, dP, color='gray', density=1, arrowstyle='->', linewidth=0.7)
-ax1.set_xlim(0, 10)
-ax1.set_ylim(0, 10)
-ax1.legend()
+# # Primer gráfico en ax1
+# ax1.plot(Na, isocline_Na, label='Isocline of Pandas', color='indigo', linewidth=2, linestyle='--')
+# ax1.plot(isocline_Pa, Pa, label='Isocline of Leopardos', color='limegreen', linewidth=2, linestyle='--')
+# ax1.set_xlabel('P')
+# ax1.set_ylabel('L')
+# ax1.plot(Na_values_1, Pa_values_1, color='deeppink', label='Trayectoria 1')
+# ax1.plot(Na_values_2, Pa_values_2, color='hotpink', label='Trayectoria 2')
+# ax1.plot(Na_values_3, Pa_values_3, color='mediumvioletred', label='Trayectoria 3')
+# ax1.set_title('Isoclines of Depredador y Presa')
+# ax1.scatter(eq1, eq1b, color='red', label='Punto de Equilibrio', s=100, zorder=10)
+# ax1.streamplot(vN, vP, dN, dP, color='gray', density=1, arrowstyle='->', linewidth=0.7)
+# ax1.set_xlim(0, 10)
+# ax1.set_ylim(0, 10)
+# ax1.legend()
 
-# Segundo gráfico en ax2
-ax2.plot(Na_values_1, label='P- Trayectoria 1', color='orangered')
-ax2.plot(Na_values_3, label='P - Trayectoria 3', color='gold')
-ax2.plot(Pa_values_1, label='L - Trayectoria 1', color='coral')
-ax2.plot(Pa_values_3, label='L - Trayectoria 3', color='khaki')
-ax2.set_xlabel('Time')
-ax2.set_ylabel('N')
-ax2.set_title('Trajectories of Predator-Prey')
-ax2.legend()
+# # Segundo gráfico en ax2
+# ax2.plot(Na_values_1, label='P- Trayectoria 1', color='orangered')
+# ax2.plot(Na_values_3, label='P - Trayectoria 3', color='gold')
+# ax2.plot(Pa_values_1, label='L - Trayectoria 1', color='coral')
+# ax2.plot(Pa_values_3, label='L - Trayectoria 3', color='khaki')
+# ax2.set_xlabel('Time')
+# ax2.set_ylabel('N')
+# ax2.set_title('Trajectories of Predator-Prey')
+# ax2.legend()
 
-# Mostrar ambos gráficos
-plt.tight_layout()
-plt.show()
+# # Mostrar ambos gráficos
+# plt.tight_layout()
+# plt.show()
 
-#Parametros part2 LVE
-#Caso 1
-Nb = np.linspace(0, 10, 10)
-Pb = np.linspace(0, 10, 10)
-rb = 0.5
-qb = 0.5
-alphab = 0.3
-betab = 0.3
-k = 10
+# #Parametros part2 LVE
+# #Caso 1
+# Nb = np.linspace(0, 10, 10)
+# Pb = np.linspace(0, 10, 10)
+# rb = 0.5
+# qb = 0.5
+# alphab = 0.3
+# betab = 0.3
+# k = 10
 
-#Isoclinas LVE
-isocline_Nlve = np.full_like(Nb, qb/betab)
-isocline_Plve = (rb/alphab) * (1 - Nb/k)
+# #Isoclinas LVE
+# isocline_Nlve = np.full_like(Nb, qb/betab)
+# isocline_Plve = (rb/alphab) * (1 - Nb/k)
 
-# Calculate the equilibrium point
-eq2xAxis = qb / betab
-eq2yAxis = (rb / alphab) * (1 - eq2xAxis / k)
+# # Calculate the equilibrium point
+# eq2xAxis = qb / betab
+# eq2yAxis = (rb / alphab) * (1 - eq2xAxis / k)
 
-#campo de vectores
-vN = np.linspace(0, 10, 40)
-vP = np.linspace(0, 10, 40)
-VN, VP = np.meshgrid(vN, vP)
+# #campo de vectores
+# vN = np.linspace(0, 10, 40)
+# vP = np.linspace(0, 10, 40)
+# VN, VP = np.meshgrid(vN, vP)
 
-dN = rb * VN * (1 - VN/k) - alphab * VN * VP
-dP = alphab * VN * VP - qb * VP
+# dN = rb * VN * (1 - VN/k) - alphab * VN * VP
+# dP = alphab * VN * VP - qb * VP
 
-#Trayectoria con runge kutta
-Nb_values_1, Pb_values_1 = rungeKuttaLotVolExt(LotkaVolterraExtODE, 5, 5, rb, alphab, betab, qb, k, 0.1, 150)
+# #Trayectoria con runge kutta
+# Nb_values_1, Pb_values_1 = rungeKuttaLotVolExt(LotkaVolterraExtODE, 5, 5, rb, alphab, betab, qb, k, 0.1, 150)
 
-# #Graficar las isoclinas
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
+# # #Graficar las isoclinas
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
 
-# Primer gráfico (Isoclinas)
-ax1.plot(isocline_Nlve, Nb, label='Isoclina de Pandas', color = 'indigo', linestyle='--', linewidth=2)
-ax1.plot(Nb, isocline_Plve, label='Isoclina de Leopardos', color = 'limegreen', linestyle='--', linewidth=2)
-ax1.set_xlabel('P')
-ax1.set_ylabel('L')
-ax1.set_title('Isoclinas de Lotka-Volterra Extendido')
-ax1.plot(Nb_values_1, Pb_values_1, color = 'violet', label='Trajectoria')
-ax1.scatter(eq2xAxis, eq2yAxis, color='red', label='Punto de Equilibrio', s=100, zorder=10)
-ax1.streamplot(vN, vP, dN, dP, color = 'gray', density=1, arrowstyle='->', linewidth=0.7)
-ax1.set_xlim(0, 10)
-ax1.set_ylim(0, 10)
-ax1.legend(loc='upper right')
+# # Primer gráfico (Isoclinas)
+# ax1.plot(isocline_Nlve, Nb, label='Isoclina de Pandas', color = 'indigo', linestyle='--', linewidth=2)
+# ax1.plot(Nb, isocline_Plve, label='Isoclina de Leopardos', color = 'limegreen', linestyle='--', linewidth=2)
+# ax1.set_xlabel('P')
+# ax1.set_ylabel('L')
+# ax1.set_title('Isoclinas de Lotka-Volterra Extendido')
+# ax1.plot(Nb_values_1, Pb_values_1, color = 'violet', label='Trajectoria')
+# ax1.scatter(eq2xAxis, eq2yAxis, color='red', label='Punto de Equilibrio', s=100, zorder=10)
+# ax1.streamplot(vN, vP, dN, dP, color = 'gray', density=1, arrowstyle='->', linewidth=0.7)
+# ax1.set_xlim(0, 10)
+# ax1.set_ylim(0, 10)
+# ax1.legend(loc='upper right')
 
-# Segundo gráfico (Trayectorias)
-ax2.plot(Nb_values_1, label='P - Trayectoria 1', color = 'violet')
-ax2.plot(Pb_values_1, label='L - Trayectoria 1', color = 'darkviolet')
-# TODO agregar otra trayectoria
-ax2.set_xlabel('Time')
-ax2.set_ylabel('N')
-ax2.set_title('Trajectories of Lotka-Volterra Extended')
-ax2.legend()
+# # Segundo gráfico (Trayectorias)
+# ax2.plot(Nb_values_1, label='P - Trayectoria 1', color = 'violet')
+# ax2.plot(Pb_values_1, label='L - Trayectoria 1', color = 'darkviolet')
+# # TODO agregar otra trayectoria
+# ax2.set_xlabel('Time')
+# ax2.set_ylabel('N')
+# ax2.set_title('Trajectories of Lotka-Volterra Extended')
+# ax2.legend()
 
-# Ajustar el diseño
-plt.tight_layout()
-plt.show()
+# # Ajustar el diseño
+# plt.tight_layout()
+# plt.show()
 
 
 
